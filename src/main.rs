bbcdeeb3157d1db1a6bb856af4bb619b5c2f7138
JonathanBrouwer <jonathantbrouwer@gmail.com>
2020-12-01 15:21:55 +0100

    Add template

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
+mod template;
 mod day1;
-mod day2;
-mod day3;
-mod day4;
-mod day5;
 
 fn main() {}
--- src/template/main.rs
+++ src/template/main.rs
@@ -1,6 +1,3 @@
-use std::collections::HashSet;
-use std::iter::FromIterator;
-
 fn part1(inp: &str) -> Result<i64, ()> {
     let input = parse_input(inp);
 
@@ -20,17 +17,29 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_part1_ex1() {
+        let result = part1(include_str!("example")).unwrap();
+        assert_eq!(0, result);
+    }
+
+    #[test]
     fn test_part1_real() {
         let result = part1(include_str!("input")).unwrap();
         println!("Part 1: {}", result);
-        assert_eq!(-1, result);
+        assert_eq!(0, result);
+    }
+
+    #[test]
+    fn test_part2_ex1() {
+        let result = part2(include_str!("example")).unwrap();
+        assert_eq!(0, result);
     }
 
     #[test]
     fn test_part2_real() {
         let result = part2(include_str!("input")).unwrap();
         println!("Part 2: {}", result);
-        assert_eq!(-1, result);
+        assert_eq!(0, result);
     }
 }
 
