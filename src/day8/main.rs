52c7c533e26342b9622e3eedc5b866cf33ebf958
JonathanBrouwer <jonathantbrouwer@gmail.com>
2020-12-08 12:10:01 +0100

    Cleanup code

--- src/day8/main.rs
+++ src/day8/main.rs
@@ -30,28 +30,45 @@ fn part2(inp: &str) -> isize {
     let mut computer = Computer::from_input(inp);
 
     //Build a graph of instructions
+    //The graph consists of 2 parts, the <S part are the regular instructions,
+    // the >S part are the instructions after changing one instruction.
+    //Each instruction that we can swap has a way to get to the S graph, but you can never get back.
     const S: isize = 10000;
     let edges = computer.instructions.iter().enumerate().flat_map(|(i, &instr)| {
         let i = i as isize;
+        //Map each instruction to edges
         match instr {
-            ACC(_) => vec![(i, i + 1), (S + i, S + i + 1)],
-            JMP(n) => vec![(i, i + n), (S + i, S + i + n), (i, S + i + 1)],
-            NOP(n) => vec![(i, i + 1), (S + i, S + i + 1), (i, S + i + n)]
+            ACC(_) => vec![
+                //Next instruction in <S part and >S part
+                (i, i + 1), (S + i, S + i + 1)],
+            JMP(n) => vec![
+                //Regular jump in <S part and >S part
+                (i, i + n), (S + i, S + i + n),
+                //We can instead make this a NOP, goes from <S part to next instruction in >S part
+                (i, S + i + 1)],
+            NOP(n) => vec![
+                //Next instruction in <S part and >S part
+                (i, i + 1), (S + i, S + i + 1),
+                //We can instead make this a jump, goes from <S part to i+n in >S part
+                (i, S + i + n)]
         }
     }).collect::<Vec<_>>();
+    //Make graph from the edges
     let graph = GraphMap::<isize, (), Directed>::from_edges(&edges);
 
-    //Run a* over the graph, find the place where we jump to the S part of the graph
+    //Run a* over the graph, to find a path to get to the final instruction
     let goal = S+(computer.instructions.len() as isize);
     let (_len, path): (_, Vec<isize>) = petgraph::algo::astar(&graph, 0, |f| f == goal, |_| 1, |_| 0).unwrap();
 
-    //Find instruction to swap
+    //Find the instruction that we swapped, the last instruction before we go to the S part
     let swap = path.windows(2).filter(|&x| x[1] >= S).next().unwrap()[0] as usize;
+    //Swap instruction in original code
     computer.instructions[swap] = match computer.instructions[swap] {
         ACC(_) => unreachable!(),
         JMP(x) => NOP(x),
         NOP(x) => JMP(x),
     };
+    //Find answer
     computer.run_until_terminate();
     return computer.acc;
 }
