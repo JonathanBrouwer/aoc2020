fbc0ef1c2b49720e2f35401b006ef0331987c6f4
JonathanBrouwer <jonathantbrouwer@gmail.com>
2020-12-08 12:05:07 +0100

    Cleanup day 8 code

--- src/day8/main.rs
+++ src/day8/main.rs
@@ -1,7 +1,10 @@
 extern crate test;
 
-use crate::day8::main::Instruction::{ACC, NOP, JMP};
 use bit_array::BitArray;
+use petgraph::{Graph, Directed};
+
+use crate::day8::main::Instruction::{ACC, JMP, NOP};
+use petgraph::graphmap::GraphMap;
 
 fn part1(inp: &str) -> isize {
     let mut computer = Computer::from_input(inp);
@@ -26,57 +29,45 @@ fn part1(inp: &str) -> isize {
 fn part2(inp: &str) -> isize {
     let mut computer = Computer::from_input(inp);
 
-    //Try flipping each possible instruction
-    for i in 0..computer.instructions.len() {
-        //Swap instruction at index i
-        let old_instr = computer.instructions[i];
-        computer.instructions[i] = match computer.instructions[i] {
-            ACC(_) => continue,
-            JMP(num) => NOP(num),
-            NOP(num) => JMP(num)
-        };
-
-        //Run and see if it terminates
-        let mut bitmap = BitArray::<u64, typenum::U1024>::from_elem(false);
-        //Loop returns if it terminated or looped
-        let did_term = loop {
-            //Step computer
-            computer.step();
-            //Did it loop?
-            if bitmap[computer.instr_counter] {
-                break false;
-            }
-            //Did it terminate?
-            if computer.has_terminated() {
-                break true;
-            }
-            //Mark this instruction as already been there
-            bitmap.set(computer.instr_counter, true);
-        };
-        //If it terminated, return the accumelator
-        if did_term {
-            return computer.acc;
+    //Build a graph of instructions
+    const S: isize = 10000;
+    let edges = computer.instructions.iter().enumerate().flat_map(|(i, &instr)| {
+        let i = i as isize;
+        match instr {
+            ACC(_) => vec![(i, i + 1), (S + i, S + i + 1)],
+            JMP(n) => vec![(i, i + n), (S + i, S + i + n), (i, S + i + 1)],
+            NOP(n) => vec![(i, i + 1), (S + i, S + i + 1), (i, S + i + n)]
         }
-        //Reset computer for another round
-        computer.reset();
-        bitmap.clear();
-        computer.instructions[i] = old_instr;
-    }
-    unreachable!();
+    }).collect::<Vec<_>>();
+    let graph = GraphMap::<isize, (), Directed>::from_edges(&edges);
+
+    //Run a* over the graph, find the place where we jump to the S part of the graph
+    let goal = S+(computer.instructions.len() as isize);
+    let (_len, path): (_, Vec<isize>) = petgraph::algo::astar(&graph, 0, |f| f == goal, |_| 1, |_| 0).unwrap();
+
+    //Find instruction to swap
+    let swap = path.windows(2).filter(|&x| x[1] >= S).next().unwrap()[0] as usize;
+    computer.instructions[swap] = match computer.instructions[swap] {
+        ACC(_) => unreachable!(),
+        JMP(x) => NOP(x),
+        NOP(x) => JMP(x),
+    };
+    computer.run_until_terminate();
+    return computer.acc;
 }
 
 #[derive(Clone)]
 struct Computer {
     acc: isize,
     instructions: Vec<Instruction>,
-    instr_counter: usize
+    instr_counter: usize,
 }
 
 #[derive(Clone, Copy)]
 enum Instruction {
     ACC(isize),
     JMP(isize),
-    NOP(isize)
+    NOP(isize),
 }
 
 impl Computer {
@@ -94,6 +85,12 @@ impl Computer {
             NOP(_num) => {
                 self.instr_counter += 1;
             }
+        }
+    }
+
+    fn run_until_terminate(&mut self) {
+        while !self.has_terminated() {
+            self.step();
         }
     }
 
@@ -125,15 +122,16 @@ impl Computer {
         return Computer {
             acc: 0,
             instr_counter: 0,
-            instructions: instr
-        }
+            instructions: instr,
+        };
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
     use test::Bencher;
+
+    use super::*;
 
     #[test]
     fn test_part1_ex1() {
